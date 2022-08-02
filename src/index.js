77867365e625a7af9995e45154f2b4e65162e0c7
rishsa <ekatesa144@gmail.com>
2022-08-02 07:47:48 +0400

    feat: sum test 8s

--- src/index.js
+++ src/index.js
@@ -38,31 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+    lenexpr = expr.length
+    masmorze = []
+    for(i=0;i<lenexpr; i+=10) {
+        masmorze.push(expr.slice(i,i+10))
+    }
+    console.log(masmorze)
+    for(i=0; i<masmorze.length; i++) {
+        if (masmorze[i]=='**********') {
+            masmorze[i]=' ';
+            continue
+        }
+        masmorze[i] = masmorze[i].slice(masmorze[i].indexOf('1'))
+        s = ''
+        console.log(masmorze[i])
+        for(j=0;j<masmorze[i].length; j+=2) {
+            if(masmorze[i].slice(j,j+2)=='11') s += '-'
+            else s += '.'
+        }
+        console.log(s)
+        masmorze[i] = MORSE_TABLE[s]
+    }
+
+    return masmorze.join('')
     // write your solution here
 }
 
 module.exports = {
     decode
 }
-// здесь тестовый код
-function decode(expr) {
-    lenexpr = expr.length
-    masmorze = []
-    for(i=0;i<lenexpr-10; i+=10) {
-        masmorze.push(expr.slice(i,i+10))
-    }
-    for(i=0; i<masmorze.length; i++) {
-        masmorze[i] = masmorze[i].slice(masmorze[i].indexOf('1'))
-        s = ''
-        for(j=0;j<masmorze[i].length; j+=2) {
-            if(masmorze[i].slice(j,j+2)=='11') s += '-'
-            else s += '.'
-        }
-        masmorze[i] = MORSE_TABLE[s]
-    }
-
-    return masmorze
-    // write your solution here
-}
-
-console.log(decode('0000101110000011111100101110100010111010000000101000000011100000111110**********00001010100011101110000011111100101111100000000010**********000010101000111011100010101010000011111100001111110010111010'))
